99ddaa962ec41a31e4c132ba5412f8f4bf1b0d61
kembaryogayogi01-web <kembaryogayogi01@gmail.com>
2025-12-22 08:47:20 +0800

    new BoxApplication

--- app/src/main/java/com/yogs/loadervip/BoxApplication.java
+++ app/src/main/java/com/yogs/loadervip/BoxApplication.java
@@ -14,36 +14,10 @@ import top.niunaijun.blackbox.app.configuration.ClientConfiguration;
 
 
 public class BoxApplication extends Application {
-    public static final String STATUS_BY = "online";
-    private native String BoxApp();
-    public static BoxApplication gApp;
-    
-    private boolean isNetworkConnected = false;
-    
-    public static BoxApplication get() {
-        return gApp;
-    }
-    
-    public boolean isInternetAvailable() {
-        return isNetworkConnected;
-    }
-    
-    public void setInternetAvailable(boolean b) {
-        isNetworkConnected = b;
-    }
-    
-    static {
-        try {
-            System.loadLibrary("Starkskillz");
-        } catch (Exception w) {
-            w.printStackTrace();
-        }
-    }
-    
+
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
-        //Thread.setDefaultUncaughtExceptionHandler(new CrashHandler(base));
         try {
             BlackBoxCore.get().doAttachBaseContext(base, new ClientConfiguration() {
                 @Override
@@ -68,52 +42,12 @@ public class BoxApplication extends Application {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        gApp = this;
-      
+
         BlackBoxCore.get().doCreate();
-
-        DynamicColors.applyToActivitiesIfAvailable(this);
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-        NetworkConnection.CheckInternet network = new NetworkConnection.CheckInternet(this);
-        network.registerNetworkCallback();
-    }
-    
-    
-    
-    /*public static boolean checkRootAccess() {
-        if (Overlay.rootAccess()){
-            FLog.info("Root granted");
-            return true;
-        } else {
-            FLog.info("Root not granted");
-            return false;
-        }
-    }*/
-    
-    public static void doExe(String shell) {
-        /*if (checkRootAccess()) {
-            Overlay.Shell(shell);
-        } else {*/
-            try {
-                Runtime.getRuntime().exec(shell);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        //}
-    }
-    
-    public void doExecute(String shell) {
-        doChmod(shell, 777);
-        doExe(shell);
-    }
-
-    public static void doChmod(String shell, int mask) {
-        doExe("chmod " + mask + " " + shell);
     }
 }
